efe943f26c18e007905ab12a3af224fc9b743c45
Gabriel Jesus <62946928+Gabriel-Jesusvix@users.noreply.github.com>
2021-10-26 09:12:16 -0300

    create Dashboard

--- src/screens/Dashboard/index.tsx
+++ src/screens/Dashboard/index.tsx
@@ -1,10 +1,35 @@
 import React from "react";
+import {} from "react-native";
 
-import { Container, Title } from "./styles";
+import {
+  Container,
+  Header,
+  UserInfo,
+  Photo,
+  User,
+  UserGreenting,
+  UserName,
+  UserWrapper,
+} from "./styles";
+
 export function Dashboard() {
   return (
     <Container>
-      <Title>Dashboard</Title>
+      <Header>
+        <UserWrapper>
+          <UserInfo>
+            <Photo
+              source={{
+                uri: "https://avatars.githubusercontent.com/u/62946928?v=4",
+              }}
+            />
+            <User>
+              <UserGreenting>Olá</UserGreenting>
+              <UserName>Gabriel J.</UserName>
+            </User>
+          </UserInfo>
+        </UserWrapper>
+      </Header>
     </Container>
   );
 }
