6a4feaf973073f33b77fa518d8ec12676fccbbbe
Gabriel Jesus <62946928+Gabriel-Jesusvix@users.noreply.github.com>
2021-10-27 08:55:38 -0300

    finished transaction card

--- src/screens/Dashboard/styles.ts
+++ src/screens/Dashboard/styles.ts
@@ -1,6 +1,10 @@
 import styled from "styled-components/native";
+import { FlatList } from "react-native";
 import { RFPercentage, RFValue } from "react-native-responsive-fontsize";
 import { getStatusBarHeight } from "react-native-iphone-x-helper";
+import { getBottomSpace } from "react-native-iphone-x-helper";
+import { DataListProps } from "../../interfaces/TransactionCardDTO";
+
 import { Feather } from "@expo/vector-icons";
 
 export const Container = styled.View`
@@ -72,5 +76,23 @@ export const HiglightCards = styled.ScrollView.attrs({
   position: absolute;
 `;
 
-export const Transactions = styled.Text``;
-export const Title = styled.Text``;
+export const Transactions = styled.View`
+  flex: 1;
+  padding: 0 24px;
+
+  margin-top: ${RFPercentage(12)}px;
+`;
+export const Title = styled.Text`
+  font-size: ${RFValue(18)}px;
+  font-family: ${({ theme }) => theme.fonts.regular};
+  margin-bottom: 16px;
+`;
+
+export const TransactionList = styled(
+  FlatList as new () => FlatList<DataListProps>
+).attrs({
+  showsVerticalScrollIndicator: false,
+  contentContainerStyle: {
+    paddingBottom: getBottomSpace(),
+  },
+})``;
