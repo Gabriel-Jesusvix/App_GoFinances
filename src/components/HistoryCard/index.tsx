1e2473108669a96ae20aff2228f632d7283746f8
Gabriel Jesus <62946928+Gabriel-Jesusvix@users.noreply.github.com>
2021-11-13 01:53:02 -0300

    Finished part resume app

---
+++ src/components/HistoryCard/index.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+import { View } from "react-native";
+
+import { Container, Title, Amount } from "./styles";
+
+interface Props {
+  color: string;
+  title: string;
+  amount: string;
+}
+export function HistoryCard({ color, title, amount }: Props) {
+  return (
+    <Container color={color}>
+      <Title>{title}</Title>
+      <Amount>{amount}</Amount>
+    </Container>
+  );
+}
--- src/screens/Dashboard/index.tsx
+++ src/screens/Dashboard/index.tsx
@@ -1,13 +1,14 @@
 import React, { useState, useEffect, useCallback } from "react";
 
+import { ActivityIndicator } from "react-native";
+
+import { useTheme } from "styled-components";
+
 import AsyncStorage from "@react-native-async-storage/async-storage";
 import { useFocusEffect } from "@react-navigation/native";
 import { HiglightCard } from "../../components/HiglightCard";
 import { TransactionCard } from "../../components/TransactionCard";
-import {
-  DataListProps,
-  TransactionCardDTO,
-} from "../../interfaces/TransactionCardDTO";
+import { DataListProps } from "../../interfaces/TransactionCardDTO";
 
 import {
   Container,
@@ -24,18 +25,63 @@ import {
   Title,
   TransactionList,
   LogoutButton,
+  LoadContainer,
 } from "./styles";
 
+interface HiglightProps {
+  amount: string;
+  lastTransaction: string;
+}
+interface HiglightDTO {
+  entries: HiglightProps;
+  expensives: HiglightProps;
+  total: HiglightProps;
+}
 export function Dashboard() {
+  const [IsLoading, setIsLoading] = useState(true);
   const [data, setData] = useState<DataListProps[]>([]);
+  const [higlightData, sethiglightData] = useState<HiglightDTO>(
+    {} as HiglightDTO
+  );
+
+  const theme = useTheme();
+
+  function getLastTransactionDate(
+    collection: DataListProps[],
+    type: "positive" | "negative"
+  ) {
+    const lastTransactions = new Date(
+      Math.max.apply(
+        Math,
+        collection
+          .filter((transaction) => transaction.type === type)
+          .map((transaction) => new Date(transaction.date).getTime())
+      )
+    );
+
+    return `${lastTransactions.getDate()} de ${lastTransactions.toLocaleString(
+      "pt-BR",
+      { month: "long" }
+    )}`;
+  }
 
   async function loadTransactions() {
     const dataKey = "@gofinances:transactions";
+
     const response = await AsyncStorage.getItem(dataKey);
     const transactions = response ? JSON.parse(response) : [];
 
+    let entriesTotal = 0;
+    let expensiveTotal = 0;
+
     const transactionsFormatted: DataListProps[] = transactions.map(
       (item: DataListProps) => {
+        if (item.type === "positive") {
+          entriesTotal += Number(item.amount);
+        } else {
+          expensiveTotal += Number(item.amount);
+        }
+
         const amount = Number(item.amount).toLocaleString("pt-BR", {
           style: "currency",
           currency: "BRL",
@@ -56,8 +102,43 @@ export function Dashboard() {
         };
       }
     );
+    const total = entriesTotal - expensiveTotal;
+
+    const lastTransactionEntries = getLastTransactionDate(
+      transactions,
+      "positive"
+    );
+    const lastTransactionExpensives = getLastTransactionDate(
+      transactions,
+      "negative"
+    );
+    const totalInterval = `01 a ${lastTransactionExpensives}`;
+    sethiglightData({
+      entries: {
+        amount: entriesTotal.toLocaleString("pt-BT", {
+          style: "currency",
+          currency: "brl",
+        }),
+        lastTransaction: `Última saída dia ${lastTransactionEntries}`,
+      },
+      expensives: {
+        amount: expensiveTotal.toLocaleString("pt-BR", {
+          style: "currency",
+          currency: "brl",
+        }),
+        lastTransaction: `Última entrada dia ${lastTransactionExpensives} `,
+      },
+      total: {
+        amount: total.toLocaleString("pt-BR", {
+          style: "currency",
+          currency: "brl",
+        }),
+        lastTransaction: totalInterval,
+      },
+    });
 
     setData(transactionsFormatted);
+    setIsLoading(false);
   }
 
   useEffect(() => {
@@ -72,53 +153,61 @@ export function Dashboard() {
 
   return (
     <Container>
-      <Header>
-        <UserWrapper>
-          <UserInfo>
-            <Photo
-              source={{
-                uri: "https://avatars.githubusercontent.com/u/62946928?v=4",
-              }}
+      {IsLoading ? (
+        <LoadContainer>
+          <ActivityIndicator color={theme.colors.primary} size="large" />
+        </LoadContainer>
+      ) : (
+        <>
+          <Header>
+            <UserWrapper>
+              <UserInfo>
+                <Photo
+                  source={{
+                    uri: "https://avatars.githubusercontent.com/u/62946928?v=4",
+                  }}
+                />
+                <User>
+                  <UserGreenting>Olá</UserGreenting>
+                  <UserName>Gabriel J.</UserName>
+                </User>
+              </UserInfo>
+              <LogoutButton onPress={() => {}}>
+                <Icon name="power" />
+              </LogoutButton>
+            </UserWrapper>
+          </Header>
+          <HiglightCards>
+            <HiglightCard
+              type="up"
+              title="Entradas"
+              amount={higlightData.entries.amount}
+              lastTransaction={higlightData.entries.lastTransaction}
             />
-            <User>
-              <UserGreenting>Olá</UserGreenting>
-              <UserName>Gabriel J.</UserName>
-            </User>
-          </UserInfo>
-          <LogoutButton onPress={() => {}}>
-            <Icon name="power" />
-          </LogoutButton>
-        </UserWrapper>
-      </Header>
-      <HiglightCards>
-        <HiglightCard
-          type="up"
-          title="Entradas"
-          amount="R$ 17.400,00"
-          lastTransaction="Última entrada dia 13 de abril"
-        />
-        <HiglightCard
-          type="down"
-          title="Saídas"
-          amount="R$ 1.259,00"
-          lastTransaction="Última saída dia 13 de abril"
-        />
-        <HiglightCard
-          type="total"
-          title="Total"
-          amount="R$ 16.141,00"
-          lastTransaction="01 à 16 de abril"
-        />
-      </HiglightCards>
+            <HiglightCard
+              type="down"
+              title="Saídas"
+              amount={higlightData.expensives.amount}
+              lastTransaction={higlightData.expensives.lastTransaction}
+            />
+            <HiglightCard
+              type="total"
+              title="Total"
+              amount={higlightData.total.amount}
+              lastTransaction={higlightData.total.lastTransaction}
+            />
+          </HiglightCards>
 
-      <Transactions>
-        <Title>Listagem</Title>
-        <TransactionList
-          data={data}
-          keyExtractor={(item) => item.id}
-          renderItem={({ item }) => <TransactionCard data={item} />}
-        />
-      </Transactions>
+          <Transactions>
+            <Title>Listagem</Title>
+            <TransactionList
+              data={data}
+              keyExtractor={(item) => item.id}
+              renderItem={({ item }) => <TransactionCard data={item} />}
+            />
+          </Transactions>
+        </>
+      )}
     </Container>
   );
 }
