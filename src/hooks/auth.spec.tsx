c08746c3ebf8e0d8b98b084bfb7cedd1de1e1cb8
Gabriel Jesus <62946928+Gabriel-Jesusvix@users.noreply.github.com>
2022-03-12 17:41:38 -0300

    correct

--- src/hooks/auth.spec.tsx
+++ src/hooks/auth.spec.tsx
@@ -1,14 +1,42 @@
+import "jest-fetch-mock";
+import { mocked } from "ts-jest/utils";
 import { renderHook, act } from "@testing-library/react-hooks";
 import { AuthProvider, useAuth } from "./auth";
+import { startAsync } from "expo-auth-session";
+import fetchMock from "jest-fetch-mock";
+
+fetchMock.enableMocks();
+
+const userTest = {
+  id: "any_id",
+  email: "john.doe@email.com",
+  name: "John Doe",
+  photo: "any_photo.png",
+};
+
+jest.mock("expo-auth-session", () => {
+  return {
+    startAsync: () => ({
+      type: "success",
+      params: {
+        access_token: "any_token",
+      },
+    }),
+  };
+});
+
+fetchMock.enableMocks();
 
 describe("Auth Hook", () => {
-  it("should be able to sign in with Google Account existing", async () => {
+  it("should be able to sign in with Google account existing", async () => {
+    fetchMock.mockResponseOnce(JSON.stringify(userTest));
+
     const { result } = renderHook(() => useAuth(), {
       wrapper: AuthProvider,
     });
-    console.log("result", result);
-    // await act(() => result.current.signInWithGoogle());
 
-    // expect(result.current.user).toBeTruthy();
+    await act(() => result.current.signInWithGoogle());
+    // expect(result.current.user.email).toBe(userTest.email);
+    expect(result.current.user).not.toHaveProperty("id");
   });
 });
